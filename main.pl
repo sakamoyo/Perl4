798a80b1c7faa3943f018add257048fa68558a8e
Your Name <you@example.com>
2018-08-04 12:39:36 +0900

    initial

--- main.pl
+++ main.pl
@@ -6,7 +6,7 @@ my $tmp = tmp();
 print($tmp);
 
 sub tmp{
-    my $system = "i'm a dog\r\ni'm a cat";
-    my $tmp = $system;
-    return $tmp;
+    my $system = "i'm a dog\r\ni'm a cat\r\nI'm a doctor";
+    my $tmp2 = $system;
+    return $tmp2;
 }
